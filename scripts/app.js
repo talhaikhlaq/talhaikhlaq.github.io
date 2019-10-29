7a60fbcf8279667277b304123b6cc8ef7349fc82
TalhaIkhlaq <talhaikhlaq@gmail.com>
2019-10-29 18:32:26 +0000

    skills page done

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,45 @@
-// console.log('Hello World')
+// function init() {
+
+let slideIndex = 1
+showSlides(slideIndex)
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n)
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+  let i
+  const slides = document.getElementsByClassName('mySlides')
+  const dots = document.getElementsByClassName('dot')
+  if (n > slides.length) {
+    slideIndex = 1 
+  }
+  if (n < 1) {
+    slideIndex = slides.length 
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '')
+  }
+  slides[slideIndex - 1].style.display = 'block'
+  dots[slideIndex - 1].className += ' active'
+}
+
+// const dotBtn = document.querySelector('.dot')
+// const prevBtn = document.querySelector('.prev')
+// const nextBtn = document.querySelector('.next')
+
+// prevBtn.addEventListener('click', plusSlides)
+// nextBtn.addEventListener('click', plusSlides)
+
+
+// }
+// window.addEventListener('DOMContentLoaded', init)
